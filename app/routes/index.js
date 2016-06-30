5d552726aa849c7aa354d53a2115c6ffc1f6c995
sly <535036822@qq.com>
2016-06-30 16:33:35 +0800

    degore

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var sly = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+sly.get('/second', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = sly;
