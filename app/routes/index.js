142880fb554ecea27aef3dd396b1c13bf26c546e
sly <535036822@qq.com>
2016-06-30 17:04:57 +0800

    merge

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var slylan = express.Router();
+var sly = express.Router();
 
 /* GET home page. */
-slylan.get('/second', function(req, res, next) {
+sly.get('/second', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = slylan;
+module.exports = sly;
